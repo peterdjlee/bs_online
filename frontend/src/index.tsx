ab8930f9c8c0fc877b35d7c25a565fe3cb566bb7
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-02-28 18:26:02 -0500

    use react-router for client side routing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Redirect, Route, BrowserRouter, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const router = (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+
+      <Route exact path="/*" render={() => <Redirect to="/" />} />
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(router, document.getElementById('root'));
