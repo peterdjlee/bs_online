6a69318f51083377f4b939b3681c017ec43e9e97
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-03-05 21:02:08 -0500

    socket provider for all components

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,17 +7,20 @@ import './index.css';
 import App from './App';
 import Join from './Join';
 import Lobby from './Lobby';
+import { socket, SocketContext } from './util/socket';
 
 const router = (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route exact path="/join" component={Join} />
-      <Route exact path="/lobby" component={Lobby} />
+  <SocketContext.Provider value={socket}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/join" component={Join} />
+        <Route exact path="/lobby" component={Lobby} />
 
-      <Route exact path="/*" render={() => <Redirect to="/" />} />
-    </Switch>
-  </BrowserRouter>
+        <Route exact path="/*" render={() => <Redirect to="/" />} />
+      </Switch>
+    </BrowserRouter>
+  </SocketContext.Provider>
 )
 
 ReactDOM.render(router, document.getElementById('root'));
