157be20b93582ddc623a4de143c40382ecdde8e7
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-04-04 16:02:47 -0400

    more ergonomic join page

--- frontend/src/routes/Join.tsx
+++ frontend/src/routes/Join.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { FormEvent, useContext, useState } from 'react';
 import { makeStyles } from '@material-ui/core';
 import { Box, Button, Typography, TextField } from '@material-ui/core';
 import { RouterProps, useParams, withRouter } from 'react-router-dom';
@@ -20,30 +20,33 @@ function Join(props: RouterProps) {
   const [nickname, setNickname] = useState('');
   const { room } = useParams<{ room: string }>();
   const player = useContext(PlayerContext);
-  const onSubmit = () => {
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
     if (nickname === '') return;
     player.nickname = nickname;
     player.room = room;
     props.history.push(`/lobby`);
   }
   return (
-    <Box
-      height="100vh"
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center">
-      <Typography className={classes.title}>Enter your name</Typography>
-      <TextField
-        className={classes.textfield}
-        label="Nickname"
-        variant="outlined"
-        onChange={event => setNickname(event.target.value)}
-        onSubmit={onSubmit} />
-      <Button variant="contained" color="primary" onClick={onSubmit}>
-        Join Game
+    <form onSubmit={onSubmit}>
+      <Box
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center">
+        <Typography className={classes.title}>Enter your name</Typography>
+        <TextField
+          className={classes.textfield}
+          label="Nickname"
+          variant="outlined"
+          autoFocus
+          onChange={event => setNickname(event.target.value)} />
+        <Button type="submit" variant="contained" color="primary">
+          Join Game
       </Button>
-    </Box>
+      </Box>
+    </form>
   )
 }
 export default withRouter(Join);
